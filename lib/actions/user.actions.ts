21fc624681a858e2eaa756fc1e9ffd2ab0da93c8
Rahul Pillai <rahulpillai81@gmail.com>
2023-10-13 14:35:25 +0530

    Added profile , theads & thread multilevel comment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,53 +1,83 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
-import { connectToDB } from "../mongoose"
+import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.models";
 
 interface Params {
-    userId : string,
-    name : string,
-    bio : string,
-    image : string,
-    path : string,
-    username : string
+  userId: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+  username: string;
 }
 
-export async function updateUser(
-    {userId , name  , bio , image , path  , username } : Params
-) : Promise<void> {
-    connectToDB();
+export async function updateUser({
+  userId,
+  name,
+  bio,
+  image,
+  path,
+  username,
+}: Params): Promise<void> {
+  connectToDB();
 
-   try {
+  try {
     await User.findOneAndUpdate(
-        {id : userId},
-        {
-            username : username.toLowerCase(),
-            name,
-            bio,
-            image,
-            onboarded : true,
-        },
-        {upsert : true}
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
     );
 
-    if(path == '/profile/edit'){
-        revalidatePath(path)
+    if (path == "/profile/edit") {
+      revalidatePath(path);
     }
-   } catch (error : any) {
-    throw new  Error('Failed ', error.message)
-   }
-
+  } catch (error: any) {
+    throw new Error("Failed ", error.message);
+  }
 }
 
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
 
-export async function fetchUser(userId : string){
-    try {
-        connectToDB();
+    return await User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error("Failed to fetch User", error);
+  }
+}
 
-        return await User
-        .findOne({id : userId})
-    } catch (error : any) {
-        throw new Error('Failed to fetch User', error)
-    }
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
+  }
 }
